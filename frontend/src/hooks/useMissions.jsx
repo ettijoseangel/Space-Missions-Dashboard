f1c6079d4760d5b6c4a3b39d4b53b68e714cd5ee
Angel <etti.joseangel@gmail.com>
2026-08-13 20:44:43 -0600

    refactor(frontend): extrae lógica de peticiones al custom hook useMissions y mejora en el manejo de errores

---
+++ frontend/src/hooks/useMissions.jsx
@@ -0,0 +1,32 @@
+import { useState, useEffect } from "react";
+
+export const useMissions = () => {
+  const [missions, setMissions] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null); // Estado para errores
+
+  useEffect(() => {
+    const fetchMissions = async () => {
+      try {
+        const response = await fetch("http://localhost:5000/api/missions");
+
+        // Verificamos si la respuesta del servidor es correcta (status 200)
+        if (!response.ok) throw new Error("Error de red o servidor");
+
+        const data = await response.json();
+        setMissions(data);
+      } catch (err) {
+        console.error("Error conectado con el backend".err);
+        setError(err.message);
+      } finally {
+        // finally se ejecuta siempre, haya error o exito
+        setLoading(false);
+      }
+    };
+
+    fetchMissions();
+  }, []);
+
+  // Hook devuelve la informacion que el componente necesita
+  return { missions, loading, error };
+};
