0a402984c6d10e1d1ef52ab4c7e7e5b400d19334
Angel <etti.joseangel@gmail.com>
2026-08-24 12:56:59 -0600

    feat(frontend): implementación de la pagina 404 para avisar de una direccion web inexistente

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,23 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { Dashboard } from "./pages/Dashboard";
 import { MissionDetail } from "./pages/MissionDetail";
-import { Toaster } from 'sonner';
+import { Toaster } from "sonner";
 import Login from "./pages/Login";
+import { NotFound } from "./pages/NotFound";
 
 function App() {
   return (
     <BrowserRouter>
-    <Toaster theme="dark" richColors position="top-right" />
-    
+      <Toaster theme="dark" richColors position="top-right" />
+
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/" element={<Navigate to="/dashboard" replace />} />
 
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/mission/:id" element={<MissionDetail />} />
+        {/* 404 HASTA EL FINAL DE ROUTES */}
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
   );
